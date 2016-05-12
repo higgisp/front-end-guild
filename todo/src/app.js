7e914d0b4c023b5873903964fb39c1578463be6f
Sam Higginbotham <higgisp@gmail.com>
2016-05-12 12:13:54 -0500

    Uses import instead of require for TodoApp

--- todo/src/app.js
+++ todo/src/app.js
@@ -1,6 +1,5 @@
 import React from 'react'
+import Todo from './components/todo-app'
 import './styles/main.styl'
-
-const Todo = require('./components/todo-app');
 
 React.render(<Todo/>, document.body)
